348da344d9d8aeed4172046f019b7e09c8da5cd5
Xala <56874379+SaharItzhak@users.noreply.github.com>
2020-11-18 18:39:51 +0200

    Add files via upload

---
+++ frilav-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAKO8dR2RKcspo1Sx0nShzHI3dmKtEbgKs",
+    authDomain: "frilav-new.firebaseapp.com",
+    databaseURL: "https://frilav-new.firebaseio.com",
+    projectId: "frilav-new",
+    storageBucket: "frilav-new.appspot.com",
+    messagingSenderId: "922503197324",
+    appId: "1:922503197324:web:b40b0ee200ebb8704281a9",
+    measurementId: "G-BGPVQH7CWR"
+  };
